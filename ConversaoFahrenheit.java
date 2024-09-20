1caff5d35da7963e8d7d60e73deafa350b08cb9d
ninica0 <santosnico048@gmail.com>
2024-09-20 15:13:27 -0300

    feat: Fiz um sistema que converte Fahrenheit para Celcius

---
+++ ConversaoFahrenheit.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ConversaoFahrenheit {
+
+	public static void main(String[] args) {
+	// Entrada 
+		int num1, num2, num3, num4, res;
+		num2 = 32;
+		num3 = 5;
+		num4 = 9;
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.print("Digite os graus em Farenheit: ");
+		num1 =  sc.nextInt();
+		res = ((num1 - num2) * num3)/num4;
+		System.out.println("O resultado em graus Celcius é: " + res);
+		
+		
+		
+		
+
+	}
+
+}
